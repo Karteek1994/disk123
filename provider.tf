37c844afe2ece4965464bc3802a92b1fb9c2c2f4
Karteek Kanithi <karteekkanithi@Karteeks-MacBook-Air.local>
2023-01-30 01:00:31 -0500

    assignment

---
+++ provider.tf
@@ -0,0 +1,10 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "646c784e-f826-42eb-9175-291641ea2ff1"
+  client_id       = "b82e576a-1592-4b92-b205-9ab9f0b3ae1c"
+  client_secret   = "WUy8Q~VDAaf6WWdl3SYicBSw5P6rsqS2BMD-laBB"
+  tenant_id       = "35c3eb03-f51d-4c82-a64f-d78c8bfbf83b"
+}
+
---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "rgname" {
+  default     = "TS-DEV-APP-DISKRG"
+  type        = string
+  description = "this is RG name"
+}
+variable "loc" {
+  default     = "East US"
+  type        = string
+  description = "this is RG Location"
+}
