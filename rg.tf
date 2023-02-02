4af534a1a0ef5eb7aa79e9ea05658adec321975a
Karteek1994 <123709104+Karteek1994@users.noreply.github.com>
2023-02-01 22:18:31 -0500

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example234" {
   name     = var.rgname
   location = var.loc
 }
 
 resource "azurerm_managed_disk" "source" {
   name                 = "TS-DEV-APP-DISK"
-  location             = azurerm_resource_group.example.location
-  resource_group_name  = azurerm_resource_group.example.name
+  location             = azurerm_resource_group.example234.location
+  resource_group_name  = azurerm_resource_group.example234.name
   storage_account_type = "Premium_LRS"
   create_option        = "Empty"
   disk_size_gb         = "100"
@@ -19,8 +19,8 @@ resource "azurerm_managed_disk" "source" {
 
 resource "azurerm_managed_disk" "copy" {
   name                 = "TS-DEV-APP-DATADISK"
-  location             = azurerm_resource_group.example.location
-  resource_group_name  = azurerm_resource_group.example.name
+  location             = azurerm_resource_group.example234.location
+  resource_group_name  = azurerm_resource_group.example234.name
   storage_account_type = "Premium_LRS"
   create_option        = "Copy"
   source_resource_id   = azurerm_managed_disk.source.id
@@ -34,8 +34,8 @@ resource "azurerm_managed_disk" "copy" {
 
 resource "azurerm_managed_disk" "copy1" {
   name                 = "TS-DEV-APP-LOGDISK"
-  location             = azurerm_resource_group.example.location
-  resource_group_name  = azurerm_resource_group.example.name
+  location             = azurerm_resource_group.example234.location
+  resource_group_name  = azurerm_resource_group.example234.name
   storage_account_type = "Premium_LRS"
   create_option        = "Copy"
   source_resource_id   = azurerm_managed_disk.source.id
@@ -49,8 +49,8 @@ resource "azurerm_managed_disk" "copy1" {
 
 resource "azurerm_managed_disk" "copy2" {
   name                 = "TS-DEV-APP-OSDISK"
-  location             = azurerm_resource_group.example.location
-  resource_group_name  = azurerm_resource_group.example.name
+  location             = azurerm_resource_group.example234.location
+  resource_group_name  = azurerm_resource_group.example234.name
   storage_account_type = "Premium_LRS"
   create_option        = "Copy"
   source_resource_id   = azurerm_managed_disk.source.id
